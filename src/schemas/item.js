02ecb79c105e4843414cd05c32a897a1001931e7
Sri <sri.kmb@gmail.com>
2025-03-25 20:18:06 -0700

    deleted unused schema

--- src/schemas/item.js
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const createItemSchema = z.object({
-  name: z.string().min(3).max(50),
-  description: z.string().optional(),
-  price: z.number().positive(),
-});
